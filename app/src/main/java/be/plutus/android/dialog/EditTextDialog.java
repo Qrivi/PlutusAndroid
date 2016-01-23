5eeb277c55252af93b50ec0061ff6852b37cd8cc
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-01-23 12:05:28 +0100

    Changed the way EditTextDialog retrieves its value

--- app/src/main/java/be/plutus/android/dialog/EditTextDialog.java
+++ app/src/main/java/be/plutus/android/dialog/EditTextDialog.java
@@ -3,20 +3,15 @@ package be.plutus.android.dialog;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import be.plutus.android.R;
 
 public class EditTextDialog extends BaseDialog
 {
-
-    /**
-     * The view of the dialog
-     */
-    private View view;
 
     /**
      * The listener that is called when the positive button is pressed
@@ -33,26 +28,13 @@ public class EditTextDialog extends BaseDialog
         this.listener = listener;
     }
 
-    /**
-     * @return The value of the text field in the dialog
-     */
-    public String getValue()
-    {
-        if ( view == null )
-            return null;
-
-        EditText editText = (EditText) view.findViewById( R.id.dialog_edit );
-        return editText.getText()
-                .toString();
-    }
-
     @Override
     public AlertDialog.Builder build()
     {
         AlertDialog.Builder builder = super.build();
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        view = inflater.inflate( R.layout.dialog_edittext, null );
+        View view = inflater.inflate( R.layout.dialog_edittext, null );
 
         builder.setView( view );
 
@@ -69,10 +51,10 @@ public class EditTextDialog extends BaseDialog
     @Override
     public void notifyPositive( DialogInterface dialog )
     {
-        Log.d( "test", ( (EditText) ( (AlertDialog) dialog ).getWindow()
-                .findViewById( R.layout.dialog_edittext ) ).getText()
+        Window window = ( (AlertDialog) dialog ).getWindow();
+        EditText editText = (EditText) window.findViewById( R.id.dialog_edit );
+        listener.onPositive( editText.getText()
                 .toString() );
-        listener.onPositive( getValue() );
     }
 
     @Override
